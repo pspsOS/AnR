2a3a3fc0ecd0212d73c823456773d028a9a242c2
thekamakaji <43650594+thekamakaji@users.noreply.github.com>
2020-12-23 23:08:13 -0500

    Created Acquisition Prototype Functions

--- MITCH_2021/Src/acquisition.c
+++ MITCH_2021/Src/acquisition.c
@@ -23,6 +23,90 @@ byte i;
 byte j;
 byte hasUpdate;
 
+
+// Setup
+/* TODO: Implement gpsSetup
+ * Established connection with GPS.
+ *
+ * Author: Jeff Kaji
+ * Date: 12/23/2020
+ */
+bool gpsSetup() {
+
+}
+
+
+/* TODO: Implement bmpSetup
+ * Established connection with Barometric Pressure Sensor.
+ *
+ * Author: Jeff Kaji
+ * Date: 12/23/2020
+ */
+bool bmpSetup() {
+
+}
+
+
+/* TODO: Implement imuSetup
+ * Established connection with IMU.
+ *
+ * Author: Jeff Kaji
+ * Date: 12/23/2020
+ */
+bool imuSetup() {
+
+}
+
+
+/* TODO: Implement alaSetup
+ * Established connection with Analog Linear Accelerometer.
+ *
+ * Author: Jeff Kaji
+ * Date: 12/23/2020
+ */
+bool alaSetup() {
+
+}
+
+// Loop
+
+
+gpsData_t getGpsData();
+
+
+/* TODO: Implement gpsRead
+ * Read data from GPS
+ *
+ * Author: Jeff Kaji
+ * Date: 12/23/2020
+ */
+gpsData_t gpsRead() {
+
+}
+
+
+/* TODO: Implement bmpRead
+ * Read data from BMP
+ *
+ * Author: Jeff Kaji
+ * Date: 12/23/2020
+ */
+bmpData_t bmpRead() {
+
+}
+
+
+/* TODO: Implement imuRead
+ * Read data from IMU and ALA
+ *
+ * Author: Jeff Kaji
+ * Date: 12/23/2020
+ */
+imuData_t imuRead();
+
+void checkStatus();
+
+//--------------------
 void getGpsData() {
 	// Check First Characters = '$G'
 	if (!(strncmp(gpsNMEA[0], "$G", 2))) {

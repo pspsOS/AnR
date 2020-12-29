a6d152233a175a18bfc7484274ded3d7c3af3729
VishnuVijay56 <75947385+VishnuVijay56@users.noreply.github.com>
2020-12-29 11:14:48 -0500

    Processing function prototypes

--- MITCH_2021/IncPSP/processing.h
+++ MITCH_2021/IncPSP/processing.h
@@ -8,6 +8,12 @@
 #ifndef INC_PROCESSING_H_
 #define INC_PROCESSING_H_
 
+/* Function prototypes */
 
+bool getBMPData();
+bool getIMUData();
+bool getGPSData();
+void processData();
+void transmitData();
 
 #endif /* INC_PROCESSING_H_ */
--- MITCH_2021/SrcPSP/processing.c
+++ MITCH_2021/SrcPSP/processing.c
@@ -6,3 +6,57 @@
  */
 
 #include "../IncPSP/processing.h"
+
+/* TODO: Implement getBMPData
+ *
+ * Author: Vishnu Vijay
+ * Date: 12/29/20
+ */
+
+bool getBMPData() {
+
+}
+
+
+/* TODO: Implement getIMUData
+ *
+ * Author: Vishnu Vijay
+ * Date: 12/29/20
+ */
+
+bool getIMUData() {
+
+}
+
+
+/* TODO: Implement getGPSData
+ *
+ * Author: Vishnu Vijay
+ * Date: 12/29/20
+ */
+
+bool getGPSData() {
+
+}
+
+
+/* TODO: Implement processData
+ *
+ * Author: Vishnu Vijay
+ * Date: 12/29/20
+ */
+
+void processData() {
+
+}
+
+
+/* TODO: Implement transmitData
+ *
+ * Author: Vishnu Vijay
+ * Date: 12/29/20
+ */
+
+void transmitData() {
+
+}

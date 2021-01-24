a24bd8457b6da97df61300e64790b920b7a3ea29
MarkBatistich <mark.batistich@yahoo.com>
2021-01-24 18:03:27 -0500

    Programmed write functions

--- MITCH_2021/IncInterface/nandInterface.h
+++ MITCH_2021/IncInterface/nandInterface.h
@@ -9,12 +9,13 @@
 #define R_BUFFER 		0x03
 
 #define W_ENABLE		0x06
+#define W_DISABLE		0x04
 #define W_LOAD			0x02
 #define W_LOAD_RAND		0x84
 #define W_EXECUTE		0x10
 
 #define GET_FEATURE		0x0F
-#define FEATURE_ADDR	0xC0;
+#define OP_FEATURE_ADDR	0xC0
 
 //Generic Defines
 #define MAX_BLOCK 		2048
@@ -27,13 +28,18 @@
 
 //Prototypes
 
-void nandBufferLoad(int32_t rowAddr); //Loads data from a row to the buffer
-void nandBufferRead(int16_t colAddr, int8_t data[], int8_t size); //Reads data from the buffer into the data array
+void nandBufferLoad(uint32_t rowAddr); //Loads data from a row to the buffer
+void nandBufferRead(uint16_t colAddr, uint8_t data[], uint8_t size); //Reads data from the buffer into the data array
 
-void nandBufferWrite(int16_t colAddr, int8_t data[], int8_t size); //Loads data from the data array to the buffer
-void nandBufferExecute(int32_t rowAddr); //Loads the data in the buffer to a row
+void nandBufferWrite(uint16_t colAddr, uint8_t data[], uint8_t size); //Loads data from the data array to the buffer
+void nandBufferExecute(uint32_t rowAddr); //Loads the data in the buffer to a row
 
-void nandRead(int32_t rowAddr, uint16_t colAddr, int8_t data[], int8_t size); //Executes nandBufferLoad and nandBufferRead
-void nandWrite(int32_t rowAddr, uint16_t colAddr, int8_t data[], int8_t size); //Executes nandBufferWrite and nandBufferExecute
+void nandRead(uint32_t rowAddr, uint16_t colAddr, uint8_t data[], uint8_t size); //Executes nandBufferLoad and nandBufferRead
+void nandWrite(uint32_t rowAddr, uint16_t colAddr, uint8_t data[], uint8_t size); //Executes nandBufferWrite and nandBufferExecute
+
+void writeEnable(); //Enables writing on the nand
+void writeDisable(); //Disables writing on the nand
+
+uint8_t getFeature(uint8_t featureAddr); //Gets a feature from the nand
 
 #endif
--- MITCH_2021/SrcInterface/nandInterface.c
+++ MITCH_2021/SrcInterface/nandInterface.c
@@ -4,16 +4,12 @@
 /**
  * @brief Load data into the buffer from the cell array
  *
- * @param data: The array to which data is read to or written from
- * @param size: The number of bytes to read/write
  * @param rowAddr: The 17 bit row address
- * @param colAddr: The 13 bit column address
  *
  * @author Mark Batistich
  * @date 01/21/2021
  */
-
-void nandBufferLoad(int32_t rowAddr){
+void nandBufferLoad(uint32_t rowAddr){
 	// Variables
 	uint8_t cmd[4];  // Command sent to device
 	uint8_t feature; //feature byte
@@ -29,19 +25,24 @@ void nandBufferLoad(int32_t rowAddr){
 		return;
 	}
 
-	cmd[0] = GET_FEATURE;
-	cmd[1] = FEATURE_ADDR;
 	do{
-		if (recieveSPI(&cmd[0], 2, &feature, 1, NAND_CS_GPIO_Port, NAND_CS_Pin, STORAGE_SPI_BUS))
-		{
-			handleHalError(BMP);
-			return;
-		}
+		feature = getFeature(OP_FEATURE_ADDR);
 		oip = getBit(feature, 0);
 	}while(oip);
 
 }
-void nandBufferRead(int16_t colAddr, int8_t data[], int8_t size){
+
+/**
+ * @brief Load data into the data array from the buffer
+ *
+ * @param data: The array to which data is read to or written from
+ * @param size: The number of bytes to read/write
+ * @param colAddr: The 13 bit column address
+ *
+ * @author Mark Batistich
+ * @date 01/24/2021
+ */
+void nandBufferRead(uint16_t colAddr, uint8_t data[], uint8_t size){
 	// Variables
 	uint8_t cmd[3];       // Command sent to device
 
@@ -56,17 +57,151 @@ void nandBufferRead(int16_t colAddr, int8_t data[], int8_t size){
 	}
 }
 
-//void nandBufferWrite(int16_t colAddr, int8_t data[], int8_t size); //Loads data from the data array to the buffer
-//void nandBufferExecute(int32_t rowAddr); //Loads the data in the buffer to a row
+/**
+ * @brief Write data into the buffer from the data array
+ *
+ * @param data: The array to which data is read to or written from
+ * @param size: The number of bytes to read/write
+ * @param colAddr: The 13 bit column address
+ *
+ * @author Mark Batistich
+ * @date 01/24/2021
+ */
+void nandBufferWrite(uint16_t colAddr, uint8_t data[], uint8_t size){
+	// Variables
+	uint8_t cmd[3];       // Command sent to device
 
-void nandRead(int32_t rowAddr, uint16_t colAddr, int8_t data[], int8_t size){
+	//Read data from the buffer into the data array
+	cmd[0] = W_LOAD_RAND;
+	cmd[2] = colAddr;
+	cmd[1] = colAddr >> 8;
+	if (sendSPI(&cmd[0], 3, NAND_CS_GPIO_Port, NAND_CS_Pin, STORAGE_SPI_BUS)){
+		handleHalError(BMP);
+		return;
+	}
+}
+
+/**
+ * @brief Write data from the buffer to the cell array
+ *
+ * @param rowAddr: The 17 bit row address
+ *
+ * @author Mark Batistich
+ * @date 01/24 /2021
+ */
+void nandBufferExecute(uint32_t rowAddr){
+	// Variables
+	uint8_t cmd[4];  // Command sent to device
+	uint8_t feature; //feature byte
+	bool oip = false; //operation in progress
+	//bool prg_f = false; //program fail
+
+	//Load data from cell array into buffer
+	cmd[0] = W_EXECUTE;
+	cmd[3] = rowAddr;
+	cmd[2] = rowAddr >> 8;
+	cmd[1] = rowAddr >> 16;
+	if (sendSPI(&cmd[0], 4, NAND_CS_GPIO_Port, NAND_CS_Pin, STORAGE_SPI_BUS)){
+		handleHalError(BMP);
+		return;
+	}
+	do{
+		feature = getFeature(OP_FEATURE_ADDR);
+		oip = getBit(feature, 0);
+		//prg_f = getBit(feature, 3);
+	}while(oip);
+}
+
+/**
+ * @brief Performs all reading commands
+ *
+ * @param rowAddr: The 17 bit row address
+ * @param data: The array to which data is read to or written from
+ * @param size: The number of bytes to read/write
+ * @param colAddr: The 13 bit column address
+ *
+ * @author Mark Batistich
+ * @date 01/24/2021
+ */
+void nandRead(uint32_t rowAddr, uint16_t colAddr, uint8_t data[], uint8_t size){
 	nandBufferLoad(rowAddr);
 	nandBufferRead(colAddr, data, size);
 }
 
-/*void nandWrite(int32_t rowAddr, uint16_t colAddr, int8_t data[], int8_t size){
+/**
+ * @brief Performs all writing commands
+ *
+ * @param rowAddr: The 17 bit row address
+ * @param data: The array to which data is read to or written from
+ * @param size: The number of bytes to read/write
+ * @param colAddr: The 13 bit column address
+ *
+ * @author Mark Batistich
+ * @date 01/24/2021
+ */
+void nandWrite(uint32_t rowAddr, uint16_t colAddr, uint8_t data[], uint8_t size){
+	nandBufferLoad(rowAddr);
+	writeEnable();
 	nandBufferWrite(colAddr, data, size);
-	nandBufferRead(rowAddr);
-}*/
+	nandBufferExecute(rowAddr);
+	writeDisable();
+}
 
+/**
+ * @brief Enables writing
+ *
+ * @author Mark Batistich
+ * @date 01/24/2021
+ */
+void writeEnable(){
+	// Variables
+	uint8_t cmd;  // Command sent to device
 
+	// Send Command
+	cmd = W_ENABLE;
+	if (sendSPI(&cmd, 1, NAND_CS_GPIO_Port, NAND_CS_Pin, STORAGE_SPI_BUS)){
+		handleHalError(BMP);
+		return;
+	}
+}
+
+/**
+ * @brief Disables writing
+ *
+ * @author Mark Batistich
+ * @date 01/24/2021
+ */
+void writeDisable(){
+	// Variables
+	uint8_t cmd;  // Command sent to device
+
+	// Send Command
+	cmd = W_DISABLE;
+	if (sendSPI(&cmd, 1, NAND_CS_GPIO_Port, NAND_CS_Pin, STORAGE_SPI_BUS)){
+		handleHalError(BMP);
+		return;
+	}
+}
+
+/**
+ * @brief Gets a feature of a specific address
+ *
+ * @param featureAddr: address of the feature to get
+ *
+ * @author Mark Batistich
+ * @date 01/24/2021
+ */
+uint8_t getFeature(uint8_t featureAddr){
+	// Variables
+	uint8_t cmd[2];  // Command sent to device
+	uint8_t feature; //feature byte
+
+	// Send Command
+	cmd[0] = GET_FEATURE;
+	cmd[1] = featureAddr;
+	if (recieveSPI(&cmd[0], 2, &feature, 1, NAND_CS_GPIO_Port, NAND_CS_Pin, STORAGE_SPI_BUS)){
+		handleHalError(BMP);
+		return 0x00;
+	}
+	return feature;
+}

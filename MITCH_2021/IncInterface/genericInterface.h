a0f4966f3ffeae127f3b6077f6d258114d85676d
thekamakaji <43650594+thekamakaji@users.noreply.github.com>
2021-02-01 14:33:08 -0500

    Added spi inclusion to generic interface

--- MITCH_2021/IncInterface/genericInterface.h
+++ MITCH_2021/IncInterface/genericInterface.h
@@ -6,6 +6,7 @@
 #include "../IncPSP/gpio.h"
 #include "stm32f4xx_hal_gpio.h"
 #include "stm32f4xx_hal_uart.h"
+#include "stm32f4xx_hal_spi.h"
 #include "stm32f4xx_hal_def.h"
 #include "stm32f4xx_hal.h"
 #include "cmsis_os.h"

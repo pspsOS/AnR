7ee2d1d2ab90ead66e1af7f69039c3af015e96c3
VishnuVijay56 <75947385+VishnuVijay56@users.noreply.github.com>
2021-02-10 16:43:32 -0500

    Completed VLQ functions

--- MITCH_2021/IncPSP/storage.h
+++ MITCH_2021/IncPSP/storage.h
@@ -10,6 +10,21 @@
 
 #include "common.h"
 
+/* Struct Typedef */
+
+typedef struct VLQ {
+	uint8_t quantityLength;
+	union {
+		uint8_t oneByteVLQ[1];
+		uint8_t twoByteVLQ[2];
+		uint8_t threeByteVLQ[3];
+		uint8_t fourByteVLQ[4];
+		uint8_t fiveByteVLQ[5];
+	};
+} VLQ_t;
+
+/* Function Prototypes */
+
 bool storageSetup();
 void storageWrite();
 
@@ -20,5 +35,10 @@ void storeBmpData();
 void storeImuData();
 void storeProcessedData();
 void storeMonitoringData();
+
+//Data Compression Functions
+
+VLQ_t convertToUVLQ(uint32_t );
+VLQ_t convertToSVLQ(int32_t );
 
 #endif /* INC_STORAGE_H_ */
--- MITCH_2021/SrcPSP/storage.c
+++ MITCH_2021/SrcPSP/storage.c
@@ -117,3 +117,101 @@ void storeProcessedData() {
 void storeMonitoringData() {
 
 }
+
+/**
+ * @brief Converts unsigned int to Unsigned VLQ
+ * Used for data compression
+ *
+ * @author Vishnu Vijay
+ * @date 2/3/21
+ */
+VLQ_t convertToUVLQ(uint32_t originalNum) {
+	uint8_t newNumArray[sizeof(originalNum) + 1] = {0};
+	uint8_t buffer = 0;
+	int length = 0;
+	do {
+		buffer = (originalNum & 0x7F);
+		if (length != 0) {
+			buffer |= 0x80;
+		}
+		newNumArray[length] = buffer;
+		length++;
+	} while ( originalNum >>= 7 );
+
+	VLQ_t newNum = {0};
+	newNum.quantityLength = length;
+	for (int i = 0; i < length; i++) {
+		switch (length) {
+			case 1:
+				newNum.oneByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+			case 2:
+				newNum.twoByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+			case 3:
+				newNum.threeByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+			case 4:
+				newNum.fourByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+			case 5:
+				newNum.fiveByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+		}
+	}
+
+	return newNum;
+}
+
+/**
+ * @brief Converts signed int to signed VLQ
+ * Used for data compression
+ *
+ * @author Vishnu Vijay
+ * @date 2/7/21
+ */
+VLQ_t convertToSVLQ(int32_t originalNum) {
+	uint8_t newNumArray[sizeof(originalNum) + 1] = {0};
+	uint8_t buffer = 0;
+	int length = 0;
+	uint8_t firstByte = 1;
+	do {
+		if (length == 0) {
+			buffer = (originalNum & 0x3F);
+			if (originalNum < 0) {
+				buffer |= 0x40;
+			}
+		}
+		else {
+			buffer = (originalNum & 0x7F);
+			buffer |= 0x80;
+			firstByte = 0;
+		}
+		newNumArray[length] = buffer;
+		length++;
+	} while ( originalNum >>= (7 - firstByte) );
+
+	VLQ_t newNum = {0};
+	newNum.quantityLength = length;
+	for (int i = 0; i < length; i++) {
+		switch (length) {
+			case 1:
+				newNum.oneByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+			case 2:
+				newNum.twoByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+			case 3:
+				newNum.threeByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+			case 4:
+				newNum.fourByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+			case 5:
+				newNum.fiveByteVLQ[i] = newNumArray[length - i - 1];
+				break;
+		}
+	}
+
+	return newNum;
+}

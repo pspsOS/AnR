813933942ade4e1fcbc6c52efc6caf9b68d70d3b
ryanhorvath00 <ryanhorvath00@users.noreply.github.com>
2021-02-11 20:22:18 -0500

    Documented debug storage testbed and fixed/added some more code

--- Debug/SrcDebug/debugStorage.c
+++ Debug/SrcDebug/debugStorage.c
@@ -5,11 +5,20 @@
  *      Author: 19rya
  */
 #include "debugStorage.h"
-
+/**
+ * @brief Writes to file
+ * Writes the pointer to a file
+ *
+ * @author Ryan Horvath
+ * @date 2/11/21
+ */
 void writeToFile(uint8_t pointer[], uint8_t size){
+	FILE *fp;
+	fp = fopen("output.txt","w+");
 	int i;
 	for(i = 0; i < size; i++){
-		fwrite(pointer[i], size, 1, 0);
+		fwrite(pointer[i], sizeof(pointer), 1, 0);
 	}
+	fclose(fp);
 }
 
